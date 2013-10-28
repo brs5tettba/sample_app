999886a8d53c066340c6e9eb72081bcc6036a08c
Ben Arunski <gitxy.20.tall@spamgourmet.com>
2013-10-28 16:55:23 -0500

    complete user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
 	# http://davidcel.is/blog/2012/09/06/stop-validating-email-addresses-with-regex/
 	# After the tutorial, replace this silly thing with the simpler one below.
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	#VALID_EMAIL_REGEX = /.+@.+\..+/i
 	validates :email, 
 		presence: true, 
@@ -16,7 +16,9 @@ class User < ActiveRecord::Base
 	validates :password, length: { minimum: 6 }
 
 	## CALLBACKS
-	before_save { self.email = email.downcase }
+	#before_save { self.email = email.downcase }
+	# use exclamation to alter a value
+	before_save { email.downcase! }
 
 	## Enable password with bcrypt-ruby
 	# Includes password and password_confirmation attributes, authenticate method, validate password presence, and require that password and password_confirmation match. -- all right out of the boy
