9478845305bbdf8cdb6cd976881c1baf1c7173bd
Ben Arunski <gitxy.20.tall@spamgourmet.com>
2013-10-29 18:34:21 -0500

    add tests, clean up flash message

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,10 @@
 module UsersHelper
 	# Returns the Gravatar (http://gravatar.com/) for the given user.
-	def gravatar_for(user)
+	def gravatar_for(user, options = { size: 50 })
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		size = options[:size]
 		gravatar_main_url = "https://secure.gravatar.com/"
-		gravatar_url =  gravatar_main_url + "avatar/#{gravatar_id}"
+		gravatar_url =  gravatar_main_url + "avatar/#{gravatar_id}?s=#{size}"
 		link_to(image_tag(gravatar_url, alt: user.name, class: "gravatar"), gravatar_main_url, target: '_blank')
 	end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,7 @@ describe "UserPages" do
 	subject { page }
 
 	describe "signup page" do
+		# visit, fill_in, click_button, etc. are Capybara verbs
 		before { visit signup_path }
 		it { should have_content('Sign up') }
 		it { should have_title(full_title('Sign up')) }
@@ -22,10 +23,69 @@ describe "UserPages" do
 		let(:submit) { "Create my account" }
 		describe "with invalid information" do
 			it "should not create a user" do
-				# click_button, fill_in, etc. are Capybara verbs
 				expect { click_button submit }.not_to change(User, :count)
 			end
-		end
+			describe "after submission" do
+				before { click_button submit }
+				it { should have_title('Sign up') }
+				it { should have_content('error') }
+			end
+
+			describe "one field is entered incorrectly" do
+				before do
+					fill_in "Name",         with: "Example User"
+					fill_in "Email",        with: "user@example.com"
+					fill_in "Password",     with: "foobar"
+					fill_in "Confirmation", with: "foobar"
+				end
+				describe "No name provided" do
+					before do
+						fill_in "Name",         with: ""
+						click_button submit
+					end
+					it { should have_content("Name can't be blank") }
+				end
+				describe "No mail provided" do
+					before do
+						fill_in "Email",        with: ""
+						click_button submit
+					end
+					it { should have_content("Email can't be blank") }
+				end
+				describe "Invalid email provided" do
+					before do
+						fill_in "Email",        with: "user@examplecom"
+						click_button submit
+					end
+					it { should have_content("Email is invalid") }
+				end
+				describe "Password is too short" do
+					before do
+						fill_in "Password",     with: "foob"
+						fill_in "Confirmation", with: "foob"
+						click_button submit
+					end
+					it { should have_content("Password is too short (minimum is 6 characters)") }
+				end
+				describe "Password is blank" do
+					before do
+						fill_in "Password",     with: ""
+						fill_in "Confirmation", with: ""
+						click_button submit
+					end
+					it { should have_content("Password can't be blank") }
+				end
+				describe "password confirmation does not match" do
+					before do
+						fill_in "Password",     with: "foobar"
+						fill_in "Confirmation", with: "foobaz"
+						click_button submit
+					end
+					it { should have_content("Password confirmation doesn't match Password") }
+				end
+			end # one field entered incorrectly
+		end # invalid information
+
 		describe "with valid information" do
 			before do
 				fill_in "Name",         with: "Example User"
@@ -36,7 +96,15 @@ describe "UserPages" do
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
 			end
-		end
+			describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+		end # valid information
+
 	end # signup
 
 end
